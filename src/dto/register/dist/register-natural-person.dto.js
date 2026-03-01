2a32d95ebe64a652d96152bdf23a0a7349775000
Nazar Kolesnikov <nazarijkolesnikov053@gmail.com>
2026-03-01 23:52:17 +0200

    Update schema.prisma___

--- src/dto/register/dist/register-natural-person.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RegisterNaturalPersonDto = void 0;
-var RegisterNaturalPersonDto = /** @class */ (function () {
-    function RegisterNaturalPersonDto() {
-    }
-    return RegisterNaturalPersonDto;
-}());
-exports.RegisterNaturalPersonDto = RegisterNaturalPersonDto;
--- src/dto/register/dist/register.seller.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.RegisterSellerDto = void 0;
-var RegisterSellerDto = /** @class */ (function () {
-    function RegisterSellerDto() {
-    }
-    return RegisterSellerDto;
-}());
-exports.RegisterSellerDto = RegisterSellerDto;
