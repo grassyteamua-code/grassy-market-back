c72ecd8d466b768bd9e0e64eab05a3591f7ae38e
Nazar Kolesnikov <nazarijkolesnikov053@gmail.com>
2026-03-03 10:37:27 +0200

    Updating Match Password Constraints class file

--- src/decorators/dist/match-password.constrains.js
+++ src/decorators/dist/match-password.constrains.js
@@ -11,6 +11,18 @@ var class_validator_1 = require("class-validator");
 var MatchPasswordConstraint = /** @class */ (function () {
     function MatchPasswordConstraint() {
     }
+    MatchPasswordConstraint.prototype.validate = function (repeatPassword, args) {
+        var password = args.object.password;
+        return password === repeatPassword;
+    };
+    MatchPasswordConstraint.prototype.defaultMessage = function (args) {
+        var password = args.object.password;
+        var repeatPassword = args.value;
+        if (password !== repeatPassword) {
+            console.log("\u041F\u0430\u0440\u043E\u043B\u044C \u0442\u0430 \u043F\u043E\u0432\u0442\u043E\u0440\u043D\u0438\u0439 \u043F\u0430\u0440\u043E\u043B\u044C \u043D\u0435 \u0437\u0456\u0432\u043F\u0430\u0434\u0430\u044E\u0442\u044C: " + password + " !== " + repeatPassword);
+        }
+        return 'Пароль та повторний пароль повинні зівпадати.';
+    };
     MatchPasswordConstraint = __decorate([
         class_validator_1.ValidatorConstraint({ async: false })
     ], MatchPasswordConstraint);
--- src/decorators/match-password.constrains.ts
+++ src/decorators/match-password.constrains.ts
@@ -1,18 +1,28 @@
 import {
-    ValidationArguments,
-    ValidatorConstraint,
-    ValidatorConstraintInterface,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
-@ValidatorConstraint({ async: false }) 
-export class MatchPasswordConstraint implements ValidatorConstraintInterface (
-    validate(repeatPassword: string, args: ValidationArguments): boolean {
-        const { password } = arg.object as any;
+@ValidatorConstraint({ async: false })
+export class MatchPasswordConstraint implements ValidatorConstraintInterface {
+  validate(repeatPassword: string, args: ValidationArguments): boolean {
+    const { password } = args.object as CreateUserDto;
 
-        return password === repeatPassword;
+    return password === repeatPassword;
+  }
+
+  defaultMessage(args: ValidationArguments): string {
+    const { password } = args.object as CreateUserDto;
+    const repeatPassword = args.value as string;
+
+    if (password !== repeatPassword) {
+      console.log(
+        `Пароль та повторний пароль не зівпадають: ${password} !== ${repeatPassword}`,
+      );
     }
 
-    defaultMessage() {
-        console.log("Пароль та повторний пароль не зівпадають.");
-    }
-)
+    return 'Пароль та повторний пароль повинні зівпадати.';
+  }
+}
