05fee1f76a78b8188089e484887dfd8dcf0655ed
Nazar Kolesnikov <nazarijkolesnikov053@gmail.com>
2026-03-03 23:13:02 +0200

    Creation Neon Serverless Postgres database and its connection to Prisma Schema and PostgreSQL

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js|jsx)$',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+};
